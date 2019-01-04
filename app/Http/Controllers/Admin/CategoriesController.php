56dfc06764ce8f4125537cd082f6a9d2a7b55308
serchern <serchern1989@gmail.com>
2019-01-04 13:57:36 +0200

    Version1.0

---
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoriesController extends Controller
+{
+    public function index()
+    {
+    	$categories = Category::all();
+
+	    return view('admin.categories.index',
+		    [
+		    	'categories' => $categories
+		    ]);
+    }
+
+    public function create()
+    {
+	    return view('admin.categories.create');
+    }
+
+    public function store(Request $request)
+    {
+    	$this->validate($request,[
+    		'title' => 'required' //обязательно к заполнению
+	    ]);
+
+		Category::create($request->all());
+		return redirect()->route('categories.index');
+    }
+
+    public function edit($id)
+    {
+		$category = Category::find($id);
+		return view('admin.categories.edit', ['category' => $category]);
+    }
+
+    public function update(Request $request, $id)
+    {
+		//dd($request->all(), $id);
+
+	    $this->validate($request,[
+		    'title' => 'required' //обязательно к заполнению
+	    ]);
+	    $category = Category::find($id);
+	    $category->update($request->all());
+//	    Category::find($id)->update($request->all());
+	    return redirect()->route('categories.index');
+    }
+
+    public function destroy($id)
+    {
+		Category::find($id)->delete();
+	    return redirect()->route('categories.index');
+    }
+}
---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use App\Tag;
+use App\Category;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+	public function index()
+	{
+		$posts = Post::paginate(2);
+
+		return view('pages.index')->with('posts', $posts);
+	}
+
+	public function show($slug)
+	{
+		$post = Post::where('slug', $slug)->firstOrFail();
+
+		return view('pages.show', compact('post'));
+	}
+
+	public function tag($slug)
+	{
+		$tag = Tag::where('slug', $slug)->firstOrFail();
+
+//		$posts = $tag->posts()->where('status', 1)->paginate(2);
+		$posts = $tag->posts()->paginate(2);
+
+		return view('pages.list', ['posts'  =>  $posts]);
+	}
+
+	public function category($slug)
+	{
+		$category = Category::where('slug', $slug)->firstOrFail();
+
+		$posts = $category->posts()->paginate(2);
+		//		$posts = $category->posts()->where('status', 1)->paginate(2);
+
+		return view('pages.list', ['posts'  =>  $posts]);
+	}
+}
